10327f8e78486b96d1f0d6cefa4624b2db119813
Murilo-ZC <carvalho.zanini@gmail.com>
2025-05-12 07:01:35 -0300

    app multi tela

---
+++ muda-tela/src/app/_layout.js
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+export default function RootLayout(){
+    return (
+        <React.Fragment>
+            <Stack></Stack>
+        </React.Fragment>
+    );
+}
+
---
+++ muda-tela/src/app/index.js
@@ -0,0 +1,21 @@
+import { Link } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Minha Tela Inicial</Text>
+      <Link href={"/segunda_tela"} push>Vai para segunda tela</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
