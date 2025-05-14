757accaccf91523d4bf14be457e4153d05a482e8
Murilo Carvalho <carvalho.zanini@gmail.com>
2025-05-14 19:28:56 -0300

    adicionado pequenas modificações no projeto multi-tela

--- muda-tela/src/app/index.js
+++ muda-tela/src/app/index.js
@@ -1,12 +1,11 @@
 import { Link } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Minha Tela Inicial</Text>
-      <Link href={"/segunda_tela"} push>Vai para segunda tela</Link>
+      <Link href={"/segunda_tela"}>Vai para segunda tela</Link>
     </View>
   );
 }
--- muda-tela/src/app/segunda_tela.js
+++ muda-tela/src/app/segunda_tela.js
@@ -1,12 +1,10 @@
 
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Minha Segunda Tela!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
