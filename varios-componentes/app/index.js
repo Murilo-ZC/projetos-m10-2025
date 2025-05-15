0532cde1b21b8eef5ad9fc6151f25bd607f7edc7
Murilo Carvalho <carvalho.zanini@gmail.com>
2025-05-15 17:27:29 -0300

    adicionado multiplas telas nos varios componentes

--- varios-componentes/app/index.js
+++ varios-componentes/app/index.js
@@ -1,32 +1,58 @@
-import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Pressable, TouchableOpacity, Alert } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Card from '../components/Card';
+import React from 'react';
+import { Picker } from '@react-native-picker/picker';
+import { useRouter } from 'expo-router';
 
-const imagens = {
-    0: "https://images.pexels.com/photos/32056657/pexels-photo-32056657/free-photo-of-black-and-white-close-up-of-a-farm-cow.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    1: "https://images.pexels.com/photos/17572427/pexels-photo-17572427/free-photo-of-cow-with-bell.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    2: "https://images.pexels.com/photos/33550/cows-curious-cattle-agriculture.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    3: "https://images.pexels.com/photos/551624/pexels-photo-551624.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    4: "https://images.pexels.com/photos/30160395/pexels-photo-30160395/free-photo-of-close-up-portrait-of-a-brown-cow-in-green-pasture.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    5: "https://images.pexels.com/photos/31669033/pexels-photo-31669033/free-photo-of-tranquil-black-and-white-swans-on-a-lake.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    6: "https://images.pexels.com/photos/6771859/pexels-photo-6771859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    7: "https://images.pexels.com/photos/17826916/pexels-photo-17826916/free-photo-of-swan-flying-above-a-lake.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    8: "https://images.pexels.com/photos/16386700/pexels-photo-16386700/free-photo-of-swans-swimming-in-the-lake.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+function chamarSegundaTela(texto, valorSelecionado){
+    // Pega o router
+    const router = useRouter();
+    if (texto.trim() === '') {
+      Alert.alert('Campo obrigatório', 'Por favor, preencha o campo.');
+    } else {
+      // Envia o app para a próxima tela
+      router.push({pathname:"/lista", params:{nome:texto, cavaleiro:valorSelecionado}});
+    }
 }
 
 export default function App() {
+    const [texto, onChangeTexto] = React.useState('');
+    const [valorSelecionado, setValorSelecionado] = React.useState('');
     return (
         <SafeAreaProvider>
             <SafeAreaView style={styles.container}>
-                <ScrollView>
-                    <Text style={styles.texto}>Meu App de Imagens!</Text>
-                    {
-                        Array.from({ length: 100 }).map((_, i) => {
-                            const url = imagens[i % Object.keys(imagens).length]; // repete ciclicamente
-                            return <Card key={i} imageUrl={url} />;
-                        })
-                    }
-                </ScrollView>
+                <View>
+                    <Text style={styles.texto}>Dados do Usuário</Text>
+                    <TextInput
+                        value={texto}
+                        placeholder='Informe seu nome'
+                        onChangeText={onChangeTexto}
+                        style={styles.input}
+                    />
+                    
+                    <Picker
+                        selectedValue={valorSelecionado}
+                        onValueChange={(itemValue, itemIndex) => {
+                            setValorSelecionado(itemValue);
+                        }}>
+                        <Picker.Item label="Áries" value="Mu" />
+                        <Picker.Item label="Touro" value="Aldebaran" />
+                        <Picker.Item label="Gêmeos" value="Saga" />
+                        <Picker.Item label="Câncer" value="Máscara da Morte" />
+                        <Picker.Item label="Leão" value="Aiolia" />
+                        <Picker.Item label="Virgem" value="Shaka" />
+                        <Picker.Item label="Libra" value="Dohko" />
+                        <Picker.Item label="Escorpião" value="Milo" />
+                        <Picker.Item label="Sagitário" value="Aiolos" />
+                        <Picker.Item label="Capricórnio" value="Shura" />
+                        <Picker.Item label="Aquário" value="Camus" />
+                        <Picker.Item label="Peixes" value="Afrodite" />
+                    </Picker>
+
+                    <TouchableOpacity style={styles.botao} onPress={()=>chamarSegundaTela(texto, valorSelecionado)}>
+                        <Text style={styles.textoBotao}>Enviar Os Dados</Text>
+                    </TouchableOpacity>
+                </View>
             </SafeAreaView>
         </SafeAreaProvider>
     );
@@ -37,10 +63,30 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
+        paddingVertical: '20',
     },
     texto: {
         fontSize: 42,
         color: '#c4c4c4',
         padding: 16,
-    }
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+    botao: {
+        padding: 8,
+        margin: 10,
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: '#f0f0f0',
+        backgroundColor: '#797979',
+        borderRadius: 20,
+    },
+    textoBotao: {
+        fontSize: 24,
+        color: '#c4c4c4',
+        padding: 16,
+    },
 });
---
+++ varios-componentes/app/lista.js
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, FlatList } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Card from '../components/Card';
+
+const imagens = {
+    0: "https://images.pexels.com/photos/32056657/pexels-photo-32056657/free-photo-of-black-and-white-close-up-of-a-farm-cow.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    1: "https://images.pexels.com/photos/17572427/pexels-photo-17572427/free-photo-of-cow-with-bell.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    2: "https://images.pexels.com/photos/33550/cows-curious-cattle-agriculture.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    3: "https://images.pexels.com/photos/551624/pexels-photo-551624.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    4: "https://images.pexels.com/photos/30160395/pexels-photo-30160395/free-photo-of-close-up-portrait-of-a-brown-cow-in-green-pasture.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    5: "https://images.pexels.com/photos/31669033/pexels-photo-31669033/free-photo-of-tranquil-black-and-white-swans-on-a-lake.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    6: "https://images.pexels.com/photos/6771859/pexels-photo-6771859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    7: "https://images.pexels.com/photos/17826916/pexels-photo-17826916/free-photo-of-swan-flying-above-a-lake.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    8: "https://images.pexels.com/photos/16386700/pexels-photo-16386700/free-photo-of-swans-swimming-in-the-lake.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+}
+
+export default function Lista() {
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container}>
+                <FlatList
+                    data={Array.from({ length: 100 }).map((_, i) =>
+                        imagens[i % Object.keys(imagens).length]
+                    )}
+                    renderItem={({item}) => (<Card imageUrl={item}/>)}
+                    ListHeaderComponent={
+                        <View>
+                            <Text style={styles.texto}>Header Da Lista</Text>
+                        </View>
+                    }
+                />
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+    },
+    texto: {
+        fontSize: 42,
+        color: '#c4c4c4',
+        padding: 16,
+    }
+});
